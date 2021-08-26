43c03746e01f359ee212757270d7d2a9d9cdf9b7
Rose Hall <rose@hall.ly>
2021-08-25 20:39:12 -0400

    Remove dep on ffi napi, and moved il2cpp.d.ts to il2cpp-js

--- index.ts
+++ index.ts
@@ -1,6 +1,13 @@
 export { SnorestopModule } from "./module";
+import { Domain } from "il2cpp";
 
 export class Snorestop {
+  constructor() {
+    Domain.get().getAssemblies().forEach(assembly => {
+      console.log(assembly.getImage());
+    });
+  }
+
   load(packageJson: any, packageIndexPath: string): void {
     new (require(packageIndexPath).default)();
   }
